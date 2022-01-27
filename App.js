805bc97f13a52a219f1b239acb307e520e3aa009
Nouman Sakhawat <nouman.sakhawat@cibaksolutions.com>
2022-01-27 18:18:37 +0500

    Animated view and Image component added

--- App.js
+++ App.js
@@ -1,30 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { useEffect } from 'react';
-import { SafeAreaView, Text,NativeModules } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import RNSplashScreen from './NativeModules/RNSplashScreen';
+import Image from './src/components/atoms/Image';
 
+const size = 330;
+export default App = () => {
 
-const App = () => {
-let svg = ``
   useEffect(() => {
     RNSplashScreen.hide();
     return () => { }
   }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "purple" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
       <Text>Jovi</Text>
+      <View style={{ height: size, width: size, backgroundColor: 'red', alignSelf: "center", borderRadius: 0, overflow: "hidden", }}>
+        <Image
+          source={{ uri: 'https://www.ppic.org/wp-content/uploads/Crowd-of-Diverse-People_800x528-768x512.jpg' }}
+        />
+      </View>
 
-    </SafeAreaView>
+    </SafeAreaView >
   );
 };
-
-
-export default App;
