ddb284f7d66bff0a6b2b38fbbd4643371f5404ee
Nouman Sakhawat <nouman.sakhawat@cibaksolutions.com>
2022-02-01 11:31:47 +0500

    Image Component Added

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,15 +1,24 @@
 import * as React from "react";
-import { Animated, Image as RNImage, StyleSheet } from "react-native";
+import { Animated, Image as RNImage, Modal, StyleSheet } from "react-native";
+import AnimatedView from "./AnimatedView";
+import FullImage from "./FullImage";
+import TouchableScale from "./TouchableScale";
 
 type Props = React.ComponentProps<typeof RNImage> & {
-  children: any;
+  children?: any;
   parentRef?: any;
   defaultIconSize?: number,
+  onPress?: () => void;
+
+  tapToOpen?: boolean;
 };
 
 const defaultProps = {
   parentRef: null,
   defaultIconSize: 100,
+  onPress: undefined,
+
+  tapToOpen: true,
 }
 
 const DEFAULT_IMAGE = require('../../assets/Logo/image_default.png');
@@ -17,57 +26,96 @@ const DEFAULT_IMAGE = require('../../assets/Logo/image_default.png');
 const Image = (props: Props) => {
   const [loading, toggleLoading] = React.useState(false);
   const [error, toggleError] = React.useState(false);
+  const [fullImage, toggleFullImage] = React.useState(false);
   const animationRef = React.useRef(new Animated.Value(0)).current;
+  const fullAnimationRef = React.useRef(new Animated.Value(0)).current;
 
   const loader = error || loading || !props.source;
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     startAnimate();
-  },[loader])
+  }, [loader])
 
   const startAnimate = () => {
-    return  Animated.timing(animationRef, {
-        duration: 1000,
-        toValue: loader ? 0 :1,
-        useNativeDriver: true,
-      }).start()
+    return Animated.timing(animationRef, {
+      duration: 1000,
+      toValue: loader ? 0 : 1,
+      useNativeDriver: true,
+    }).start()
   };
+
+  const startFullAnimate = (val: number) => {
+    return Animated.timing(fullAnimationRef, {
+      duration: 800,
+      toValue: val,
+      useNativeDriver: true,
+    }).start(() => {
+      if (val === 0)
+        toggleFullImage(false);
+    })
+  };//end of startFullAnimate
 
   return (
     <>
-    
-      <Animated.Image
-        style={[
-          {
-            opacity: animationRef,
-          },
-           styles.image,
-          props.style]}
+      <TouchableScale onPress={() => {
+        if (props.onPress) {
+          props.onPress();
+          return
+        }
+        startFullAnimate(1);
+        toggleFullImage(true);
+      }}
+        disabled={loader || !props.tapToOpen}>
 
-        onLoadStart={() => toggleLoading(true)}
-        onLoadEnd={() => toggleLoading(false)}
-        onError={() => toggleError(true)}
+        <Animated.Image
+          style={[
+            {
+              opacity: animationRef,
+            },
+            styles.image,
+            props.style]}
 
-        defaultSource={DEFAULT_IMAGE}
+          onLoadStart={() => toggleLoading(true)}
+          onLoadEnd={() => {toggleLoading(false); toggleError(false)}}
+          onError={() => toggleError(true)}
 
-        {...props}
-      />
+          defaultSource={DEFAULT_IMAGE}
+
+          {...props}
+        />
+      </TouchableScale>
+
 
 
       {/* //WHEN ERROR OR LOADING */}
-      <RNImage
-        {...props}
-        style={[
-          loader ? {
-            ...styles.defaultImage,
-            height: props.defaultIconSize,
-            width: props.defaultIconSize,
-          } : {
-            opacity:0,
-          }, props.style]}
-        source={DEFAULT_IMAGE}
-      />
+      {loader &&
+        <RNImage
+          {...props}
+          style={[
+            loader ? {
+              ...styles.defaultImage,
+              height: props.defaultIconSize,
+              width: props.defaultIconSize,
+              position: 'absolute',
 
+              borderColor: '#140c0c19',
+              borderWidth: 0.5,
+              borderRadius: 6,
+            } : {
+              opacity: 0,
+              overflow: 'hidden',
+            }, props.style]}
+          source={DEFAULT_IMAGE}
+        />
+      }
+
+{fullImage && 
+    <FullImage
+      source={props.source}
+      toggleFullImage={() => {
+        startFullAnimate(0);
+      }} />
+}
     </>
 
   );
@@ -77,12 +125,12 @@ export default Image;
 
 const styles = StyleSheet.create({
   image: {
-    flex: 1,
+    // flex: 1,
     ...StyleSheet.absoluteFillObject,
     resizeMode: "cover",
   },
   defaultImage: {
-    flex: 1,
+    // flex: 1,
     height: 100,
     width: 100,
     alignSelf: "center",
