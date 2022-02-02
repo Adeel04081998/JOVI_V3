e2e461c5beb2efd7d152bb856b8fc586f9f0fd14
Nouman Sakhawat <nouman.sakhawat@cibaksolutions.com>
2022-02-02 11:56:45 +0500

    DraggableFlatList Added

---
+++ src/components/molecules/DraggableFlatList.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import RNDraggableFlatList, {
+  RenderItem,
+  RenderItemParams,
+  ScaleDecorator
+} from "react-native-draggable-flatlist";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+type Props = React.ComponentProps<typeof RNDraggableFlatList> & {
+  children?: any;
+  updateData?: (data: any) => void;
+  renderItem?: RenderItem<any>;
+
+};
+
+const defaultProps = {
+  updateData: undefined,
+}
+
+const DraggableFlatList = (props: Props) => {
+  const activatedIndex = React.useRef(-1);
+  const [activated, setActivated] = useState(false);
+
+  const MyrenderItem = (itemProps: RenderItemParams<unknown>) => {
+    return <ScaleDecorator>
+      <TouchableOpacity
+        onLongPress={itemProps.drag}
+        disabled={itemProps.isActive}
+        style={itemProps.isActive ? {
+    backgroundColor: "#fff",
+          opacity: 1,
+        } : {
+          backgroundColor:activatedIndex.current!==-1 ? `rgba(0,0,0,0.3)` : '#fff',
+        }}>
+        {/* @ts-ignore */}
+        <props.renderItem {...itemProps} />
+      </TouchableOpacity>
+    </ScaleDecorator>
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+
+      <RNDraggableFlatList
+        {...props}
+        keyExtractor={(item, index) => `${index}`}
+        extraData={activated}
+        onDragBegin={(index) => {
+          activatedIndex.current = index;
+          setActivated(true);
+        }}
+        onDragEnd={({ data }) => {
+          activatedIndex.current = -1;
+          setActivated(false);
+          props.updateData && props.updateData(data)
+        }}
+
+        renderItem={(itemProps) => <MyrenderItem {...itemProps} />}
+      />
+    </GestureHandlerRootView>
+
+  );
+}
+
+DraggableFlatList.defaultProps = defaultProps;
+export default DraggableFlatList;
