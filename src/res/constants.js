d8edcf437c294a8e7ce0ee07b91e8c16c0f3b537
Nouman Sakhawat <nouman.sakhawat@cibaksolutions.com>
2022-02-02 18:53:41 +0500

    ImageCarousel Added

--- src/res/constants.js
+++ src/res/constants.js
@@ -1,5 +1,7 @@
 import { Dimensions } from "react-native";
 export default {
     SCREEN_DIMENSIONS: Dimensions.get("screen"),
-    WINDOW_DIMENSIONS: Dimensions.get("window")
+    WINDOW_DIMENSIONS: Dimensions.get("window"),
+
+    HORIZONTAL_MARGIN:10,
 }
