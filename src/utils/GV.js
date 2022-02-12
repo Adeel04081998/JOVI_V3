19d22bedffc59e1cf0de3b60f53d423e8a5f44ef
Mudassir Malik <mudassir@cibaksolutions.com>
2022-02-12 17:08:57 +0500

    some updates

--- src/utils/GV.js
+++ src/utils/GV.js
@@ -2,20 +2,20 @@ import React from "react";
 import constants from "../res/constants";
 let initUrlRef = React.createRef(null);
 initUrlRef.current = __DEV__ ? constants.url_dev : constants.url_live;
+export const PITSTOP_TYPES = {
+    DEFAULT: 0,
+    SUPER_MARKET: 1,
+    JOVI: 2,
+    PHARMACY: 3,
+    RESTAURANT: 4,
+    JOVI_MART: 5,
+};
 export default {
     BASE_URL: initUrlRef,
-    netInfoRef: React.createRef(null),
     SET_VALUE: 1,
     GET_VALUE: 2,
     NET_INFO_REF: React.createRef(null),
     OTP_INTERVAL: 30, // SECONDS
-    THEME_VALUES: {
-        DEFAULT: 0,
-        SUPER_MARKET: 1,
-        JOVI: 2,
-        PHARMACY: 3,
-        RESTAURANT: 4,
-        JOVI_MART: 5,
-    },
+    THEME_VALUES: { ...PITSTOP_TYPES },
 
 } 
