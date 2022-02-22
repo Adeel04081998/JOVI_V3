36f896293e89a025ecea6640a2f933a19fd2cc37
ADEEL FAREED <adeel.fareed@cibaksolutions.com>
2022-02-22 11:34:29 +0500

    update

--- src/screens/Home/components/Search.js
+++ src/screens/Home/components/Search.js
@@ -3,14 +3,16 @@ import Text from '../../../components/atoms/Text';
 import TextInput from '../../../components/atoms/TextInput';
 import VectorIcon from '../../../components/atoms/VectorIcon';
 import View from '../../../components/atoms/View';
-export default ({ colors, homeStyles,onSearch=()=>{}, placeholder=null }) => {
+export default ({ colors, homeStyles,onSearch=()=>{}, placeholder=null, searchProps={} }) => {
     const [state,setState] = React.useState({
         value:'',
     });
     return (
         <View style={homeStyles.search_container}>
             <VectorIcon name='search' style={{ left: 10 }} color={colors.primary} onPress={()=>onSearch(state.value)} />
-            <TextInput textAlign="left" placeholder={placeholder??'Search for shops and restaurants or pharmacy'} onChangeText={(val)=>setState(pre=>({...pre,value:val}))} style={homeStyles.search_input} />
+            <TextInput value={state.value} {...searchProps} textAlign="left" onSubmitEditing={()=>onSearch(state.value)} placeholder={placeholder??'Search for shops and restaurants or pharmacy'} onChangeText={(val)=>setState(pre=>({...pre,value:val}))} style={homeStyles.search_input} 
+            maxLength={45}
+            />
         </View>
     );
 }
