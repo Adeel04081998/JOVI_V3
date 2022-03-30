4f7b77896fde467f074fa8f68a074866c291423e
Malik Awais <awais@cibaksolutions.com>
2022-03-30 13:00:10 +0500

    styles separated for goody bag

--- src/screens/GoodyBag/index.js
+++ src/screens/GoodyBag/index.js
@@ -21,7 +21,6 @@ const ICON_CONTAINER_SIZE = 40;
 export default () => {
     const colors = theme.getTheme(GV.THEME_VALUES[PITSTOP_TYPES_INVERTED[PITSTOP_TYPES.JOVI]], Appearance.getColorScheme() === "dark");
     const customheaderStyles = { ...CustomHeaderStyles(colors.primary) };
-    const _styles = styles(colors);
     const [state, setState] = React.useState({
         data: []
     });
@@ -78,37 +77,19 @@ export default () => {
         </SafeAreaView>
     );
 };
-
-const styles = (colors) => StyleSheet.create({
-    primaryContainer: {
-        flex: 1,
-        backgroundColor: colors.white,
-    },
-    contactUsBlock: {
-        height: 95,
-        width: 95,
-        borderRadius: 12,
-        borderWidth: 0.4,
-        paddingTop: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    contactUsText: { fontSize: 14, color: colors.black },
-    contactUsTitle: { fontSize: 18, color: colors.black },
-    iconContainer: { height: ICON_CONTAINER_SIZE, width: ICON_CONTAINER_SIZE, marginVertical: 5, justifyContent: 'center', alignItems: 'center', borderRadius: ICON_CONTAINER_SIZE / 2, backgroundColor: colors.primary }
-});
 const Voucher = ({ parentScrollHandler = () => { }, item = {}, index = {}, colors = {} }) => {
     const [state, setState] = React.useState({
         opened: false
     });
+    const styles = voucherStyles(colors);
     const renderVoucherWebView = () => {
         if (state.opened) {
             return (
-                <View style={{ marginHorizontal: 23, bottom: Platform.OS === 'android' ? 5 : 2, height: 200, }}>
+                <View style={styles.voucherDetailsContainer}>
                     <CustomWebView
                         key={`customer_promo_key_${index}`}
                         html={item.promoHtml}
-                        screenStyles={{ width: '100%', minHeight: 200, height: 200, backgroundColor: "#fff", }}
+                        screenStyles={styles.voucherWebViewScreen}
                         webViewProps={{
                             injectedJavaScript: "document.body.style.userSelect = 'none'",
                             onTouchStart: () => {
@@ -131,17 +112,16 @@ const Voucher = ({ parentScrollHandler = () => { }, item = {}, index = {}, color
     }
     return (
         <View>
-            <VoucherUi colors={colors} title={item.title} description={item.promoCode} discountPercentage={'10%'} discountValidityDate={item.expiryTime} maxDiscount={item.discountCap} voucherOpened={state.opened} onPress={() => { setState(pre => ({ ...pre, opened: !pre.opened })); console.log('voucherPressed', state.opened); }} />
+            <VoucherUi colors={colors} title={item.title} description={item.promoCode} discountPercentage={item.discountValue ?? ''} discountValidityDate={item.expiryTime} maxDiscount={item.discountCap} voucherOpened={state.opened} onPress={() => { setState(pre => ({ ...pre, opened: !pre.opened })); console.log('voucherPressed', state.opened); }} />
             {renderVoucherWebView()}
         </View>
     );
 }
 const VoucherUi = ({ title = "", colors = {}, voucherOpened = false, discountPercentage = "", description = "", discountCategory = "", maxDiscount = "", discountValidity = "", discountValidityDate = '', onPress = () => { }, activeTheme = {}, arcBackground = "", style, promoCodeApplied = "" }) => {
+    const styles = voucherUIStyles(colors);
     return (
-        <View style={{ position: 'relative', paddingTop: 10, height: 100, }}>
-            <View style={{
-                flexDirection: "column", marginHorizontal: 20, bottom: 10,
-            }}>
+        <View style={styles.container}>
+            <View style={styles.svgContainer}>
                 <SvgXml xml={voucherOpened ? svgs.voucherUISelected() : svgs.voucherUI()} width={'100%'} />
             </View>
             <TouchableOpacity onPress={onPress} style={{
@@ -151,28 +131,28 @@ const VoucherUi = ({ title = "", colors = {}, voucherOpened = false, discountPer
                 marginHorizontal: 20,
                 marginTop: 8,
             }}>
-                <View style={{ height: '55%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingVertical: 5 }}>
+                <View style={styles.voucherTopContainer}>
                     <View style={{ width: '95%', justifyContent: 'space-between', flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 15, color: colors.black, maxWidth: '90.3%' }} fontFamily={'PoppinsBold'} numberOfLines={1}>{title}</Text>
-                        <Text style={{ fontSize: 15, color: colors.black }} fontFamily={'PoppinsBold'}>{discountPercentage}</Text>
+                        <Text style={{ ...styles.topText, maxWidth: '90.3%' }} fontFamily={'PoppinsBold'} numberOfLines={1}>{title}</Text>
+                        <Text style={{ ...styles.topText, }} fontFamily={'PoppinsBold'}>{discountPercentage}</Text>
                     </View>
                     <View style={{ width: '95%' }}>
-                        <Text style={{ fontSize: 10, color: colors.black }} numberOfLines={1}>{description}</Text>
+                        <Text style={styles.lowerText} numberOfLines={1}>{description}</Text>
                     </View>
                 </View>
                 <View style={{ height: '45%', paddingTop: 5, paddingLeft: 10 }}>
-                    <DashedLine containerStyles={{ top: -7, position: 'absolute', width: '97%', alignSelf: 'center' }} />
+                    <DashedLine containerStyles={styles.lineSeparator} />
                     <View style={{ width: '95%', justifyContent: 'flex-end', flexDirection: 'row' }}>
                         <Text style={{ fontSize: 10, color: colors.primary, }} >T&C</Text>
                     </View>
                     <View style={{ width: '95%', justifyContent: 'space-between', flexDirection: 'row' }}>
                         <View style={{ flexDirection: 'row' }}>
-                            <Text style={{ fontSize: 10, color: colors.black }}>Max Disc: </Text>
-                            <Text style={{ fontSize: 10, color: colors.black }} fontFamily={'PoppinsBold'}>{renderPrice(maxDiscount)}</Text>
+                            <Text style={styles.lowerText}>Max Disc: </Text>
+                            <Text style={styles.lowerText} fontFamily={'PoppinsBold'}>{renderPrice(maxDiscount)}</Text>
                         </View>
                         <View style={{ flexDirection: 'row' }}>
-                            <Text style={{ fontSize: 10, color: colors.black }}>Valid until </Text>
-                            <Text style={{ fontSize: 10, color: colors.black }} fontFamily={'PoppinsBold'} numberOfLines={1}>{discountValidityDate}</Text>
+                            <Text style={styles.lowerText}>Valid until </Text>
+                            <Text style={styles.lowerText} fontFamily={'PoppinsBold'} numberOfLines={1}>{discountValidityDate}</Text>
                         </View>
                     </View>
                 </View>
@@ -180,3 +160,19 @@ const VoucherUi = ({ title = "", colors = {}, voucherOpened = false, discountPer
         </View>
     )
 }
+
+const voucherStyles = (colors) => StyleSheet.create({
+    voucherDetailsContainer: { marginHorizontal: 23, bottom: Platform.OS === 'android' ? 5 : 2, height: 200, },
+    voucherWebViewScreen: { width: '100%', minHeight: 200, height: 200, backgroundColor: "#fff", },
+});
+const voucherUIStyles = (colors) => StyleSheet.create({
+    container: { position: 'relative', paddingTop: 10, height: 100, },
+    svgContainer: {
+        flexDirection: "column", marginHorizontal: 20, bottom: 10,
+    },
+    voucherTopContainer:{ height: '55%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingVertical: 5 },
+    lowerText:{ fontSize: 10, color: colors.black },
+    lineSeparator:{ top: -7, position: 'absolute', width: '97%', alignSelf: 'center' },
+    topText:{fontSize: 15, color: colors.black},
+
+});
