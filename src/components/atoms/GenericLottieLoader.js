14c3b56db2a3192d4e26cec2a879adce19f862bd
ADEEL FAREED <adeel.fareed@cibaksolutions.com>
2022-04-29 14:46:40 +0500

    Add Generic loader in remaining screen

--- src/components/atoms/GenericLottieLoader.js
+++ src/components/atoms/GenericLottieLoader.js
@@ -1,11 +1,8 @@
 import AnimatedLottieView from 'lottie-react-native';
 import React, { useEffect } from 'react';
-import { Animated, Easing, TouchableOpacity } from 'react-native';
-import { SvgXml, Text } from 'react-native-svg';
 import View from './View';
 
 export default (props) => {
-
     return (
         <View style={[{
             flex: 1,
